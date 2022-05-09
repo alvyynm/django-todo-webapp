29a392e68ef04263e2a1bb035c60d27a2d07a1b7
alvyynm <alvyynm@gmail.com>
2022-05-09 08:21:05 +0300

    Redirect user to login page after successful account creation

--- users/views.py
+++ users/views.py
@@ -9,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account for { username } created successfully!")
-            return redirect('task-home') 
+            messages.success(request, f"Account for { username } created successfully!. You can now login.")
+            return redirect('login') 
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
