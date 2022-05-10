eea22a0f4a71c8bb0d1a9ca95fdd7d2529b9cc5a
alvyynm <alvyynm@gmail.com>
2022-05-10 12:42:20 +0300

    Add min and max acceptable values to task_priority

--- task/models.py
+++ task/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Task(models.Model):
     task_name = models.CharField(max_length=40)
     task_description = models.TextField()
     task_due_date = models.DateField()
-    task_priority = models.IntegerField()
+    task_priority = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     # def __str__(self):
     #     self.task_name
